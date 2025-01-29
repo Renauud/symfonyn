66059d0173f1194bcb37c039dc71018cc59aa0a8
renaudLapTop <rzell57@gmail.com>
2025-01-29 09:41:28 +0100

    debut filesystem / taskfileservice

---
+++ src/Services/TaskFileService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+class TaskFileService
+{
+    
+}
